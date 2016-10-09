4b25863525e327e5acd69360d012b853d8dc03db
madeindjs <madeindjs@gmail.com>
2016-10-09 15:45:45 +0200

    get the title using nokogiri gem

--- lib/marmiton_crawler.rb
+++ lib/marmiton_crawler.rb
@@ -1,17 +1,21 @@
-require 'net/http'
+require 'open-uri'
+require 'nokogiri'
 
 require "marmiton_crawler/version"
+
 
 module MarmitonCrawler
 
   class Recipe
 
+    attr_reader :title
+
     MARMITON_HOST = 'http://www.marmiton.org/'
 
     def initialize(url)
       if url.include? MARMITON_HOST
-        @url = url
-        @html =  Net::HTTP.get URI(@url)
+        @page =  Nokogiri::HTML(open(url))
+        @title = @page.css('h1.m_title span.item span.fn').text
       else
         raise ArgumentError, "Instantiation cancelled (ulr not from #{MARMITON_HOST})." 
       end
--- spec/marmiton_crawler_spec.rb
+++ spec/marmiton_crawler_spec.rb
@@ -1,18 +1,29 @@
 require 'spec_helper'
 
 describe MarmitonCrawler do
+
+  before(:each) do
+    marmiton_url = 'http://www.marmiton.org/recettes/recette_burger-d-avocat_345742.aspx'
+    @recipe = MarmitonCrawler::Recipe.new marmiton_url
+  end
+
   it 'has a version number' do
     expect(MarmitonCrawler::VERSION).not_to be nil
   end
 
-  it 'should instanciante  this ' do
-    url = 'http://www.marmiton.org/recettes/recette_burger-d-avocat_345742.aspx'
-    expect(MarmitonCrawler::Recipe.new url).not_to be nil
+  it 'should create the recipe' do
+    expect(@recipe).not_to be nil
   end
 
-  it 'should not instanciante  this ' do
+  it 'should not instanciante the recipe because it\'s not a marmiton.org url' do
     url = 'http://www.google.fr'
     expect{MarmitonCrawler::Recipe.new url}.to raise_error(ArgumentError)
   end
+
+  it 'should get the right title' do
+    expect(@recipe.title).to eq("Burger d'avocat")
+  end
+
+
 
 end

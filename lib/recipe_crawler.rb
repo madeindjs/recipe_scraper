32a8b9208147719fb98de4d00961f7ce2f06b1e9
madeindjs <madeindjs@gmail.com>
2016-11-28 17:19:37 +0100

    implement 750g future methods & add YARD documentation

--- lib/recipe_crawler.rb
+++ lib/recipe_crawler.rb
@@ -8,25 +8,33 @@ require "recipe_crawler/version"
 
 module RecipeCrawler
 
+  # represent a recipe fetched from an Url
   class Recipe
 
     attr_reader :title, :preptime, :cooktime , :ingredients, :steps, :image
 
     MARMITON_HOST = {desktop: 'http://www.marmiton.org/', mobile: 'http://m.marmiton.org/'}
+    G750_HOST = 'http://www.750g.com'
 
 
     
-
+    # Instanciate a Recipe object with data crawled from an url
+    #
+    # @param url [String] representing an url from Marmiton or 750g website
     def initialize url
       if marmiton_host? url
         fetch_from_marmiton url
+      elsif g750_host? url
+        fetch_from_g750 url
       else
         raise ArgumentError, "Instantiation cancelled (Host not supported)." 
       end
     end
 
 
-    # export all informations to an array
+    # export object properties to hash
+    #
+    # @return [Hash] as object's properties
     def to_hash
       attrs = Hash.new
       instance_variables.each do |var|
@@ -37,6 +45,9 @@ module RecipeCrawler
     end
 
 
+    # convert object properties to json
+    #
+    # @return [String] data formated in JSON
     def to_json
       return self.to_hash.to_json
     end
@@ -44,17 +55,37 @@ module RecipeCrawler
 
     private
 
+
     # remove `\r\n` & unwanted espaces
+    #
+    # @param text [String] a text to sanitize
+    # @return [String] as text corrected formated
     def sanitize text
       ['  ', '\r\n', "\r\n"].each { |text_to_remove| text.gsub!(text_to_remove,'')}
       return text
     end
 
+    # test if url is from a valid marmiton.org host
+    #
+    # @param url [String] representing an url
+    # @return [Boolean] as true if coresponding to a valid url
     def marmiton_host? url
       return url.include?(MARMITON_HOST[:desktop]) || url.include?(MARMITON_HOST[:mobile])
     end
 
-    # get data from an marmiton url
+
+    # test if url is from a valid 750g.com host
+    #
+    # @param url [String] representing an url
+    # @return [Boolean] as true if coresponding to a valid url
+    def g750_host? url
+      return url.include? G750_HOST
+    end
+
+
+    # fill object properties from a Marmiton url
+    #
+    # @param url [String] representing an url
     def fetch_from_marmiton url
       if marmiton_host? url
 
@@ -83,6 +114,18 @@ module RecipeCrawler
 
       else
         raise ArgumentError, "Instantiation cancelled (ulr not from #{MARMITON_HOST})." 
+      end
+    end
+
+
+    # fill object properties from a 750g url
+    #
+    # @param url [String] representing an url
+    def fetch_from_g750 url
+      if g750_host? url
+          # TODO: add logic here
+      else
+        raise ArgumentError, "Instantiation cancelled (ulr not from #{G750_HOST})." 
       end
     end
   

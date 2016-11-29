31a2748a4e0a7997c0cb55942e126c779bad13b1
madeindjs <madeindjs@gmail.com>
2016-11-29 12:54:16 +0100

    clarify tests

--- spec/recipe_750g_spec.rb
+++ spec/recipe_750g_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe RecipeCrawler do
+describe "http://www.750g.com recipe crawler" do
 
   before(:each) do
     marmiton_url = 'http://www.750g.com/tarte-amandine-aux-pommes-dapi-r30157.htm'
@@ -8,13 +8,13 @@ describe RecipeCrawler do
   end
 
 
-  it 'should instanciante a recipe from 750g' do
+  it 'should instanciante a recipe' do
     expect(@recipe_750g).not_to be nil
   end
 
 
 
-  it 'should get the right title from 750g' do
+  it 'should get the right title' do
     expect(@recipe_750g.title).to eq("Tarte Amandine aux Pommes d'Api")
   end
 
@@ -42,20 +42,6 @@ describe RecipeCrawler do
     expect(bretzel.image).to eq src="http://static.750g.com/images/auto-427/f3aa5fb6bb6f1059c4119c5b4cb0d720/bretzel.jpeg"
   end
 
-
-
-  it 'should export all informations to json' do
-    exepted_string = "{\"title\":\"Burger d'avocat\",\"preptime\":20,\"cooktime\":7,\"ingredients\":[\"2 beaux avocat\",\"2 steaks hachés de boeuf\",\"2 tranches de cheddar\",\"quelques feuilles de salade\",\"1/2 oignon rouge\",\"1 tomate\",\"graines de sésame\",\"1 filet d'huile d'olive\",\"1 pincée de sel\",\"1 pincée de poivre\"],\"steps\":[\"Laver et couper la tomate en rondelles\",\"Cuire les steaks à la poêle avec un filet d'huile d'olive\",\"Saler et poivrer\",\"Toaster les graines de sésames\",\"Ouvrir les avocats en 2, retirer le noyau et les éplucher\",\"Monter les burger en plaçant un demi-avocat face noyau vers le haut, déposer un steak, une tranche de cheddar sur le steak bien chaud pour qu'elle fonde, une rondelle de tomate, une rondelle d'oignon, quelques feuilles de salade et terminer par la seconde moitié d'avocat\",\"Parsemer quelques graines de sésames.\"],\"image\":\"http://images.marmitoncdn.org/recipephotos/multiphoto/7b/7b4e95f5-37e0-4294-bebe-cde86c30817f_normal.jpg\"}"
-    expect(@recipe_750g.to_json).to eq exepted_string
-  end
-
-
-  # it 'should convert a m.marmiton.org url into a valid url' do
-  #   url = 'http://m.marmiton.org/recettes/recette_burger-d-avocat_345742.aspx'
-  #   recipe = RecipeCrawler::Recipe.new url
-  #   expect(recipe).not_to be nil
-  #   expect(recipe.title).to eq("Burger d'avocat")
-  # end
 
 
 end
--- spec/recipe_marmiton_spec.rb
+++ spec/recipe_marmiton_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe RecipeCrawler do
+describe "http://www.marmiton.org recipe crawler" do
 
   before(:each) do
     marmiton_url = 'http://www.marmiton.org/recettes/recette_burger-d-avocat_345742.aspx'
@@ -8,20 +8,10 @@ describe RecipeCrawler do
   end
 
 
-  it 'has a version number' do
-    expect(RecipeCrawler::VERSION).not_to be nil
-  end
-
-
   it 'should create the recipe' do
     expect(@recipe).not_to be nil
   end
 
-
-  it 'should not instanciante the recipe because it\'s not a marmiton.org url' do
-    url = 'http://www.google.fr'
-    expect{RecipeCrawler::Recipe.new url}.to raise_error(ArgumentError)
-  end
 
 
   it 'should get the right title' do
@@ -65,11 +55,6 @@ describe RecipeCrawler do
     expect(@recipe.to_hash).to eq exepted_hash
   end
 
-
-  it 'should export all informations to json' do
-    exepted_string = "{\"title\":\"Burger d'avocat\",\"preptime\":20,\"cooktime\":7,\"ingredients\":[\"2 beaux avocat\",\"2 steaks hachés de boeuf\",\"2 tranches de cheddar\",\"quelques feuilles de salade\",\"1/2 oignon rouge\",\"1 tomate\",\"graines de sésame\",\"1 filet d'huile d'olive\",\"1 pincée de sel\",\"1 pincée de poivre\"],\"steps\":[\"Laver et couper la tomate en rondelles\",\"Cuire les steaks à la poêle avec un filet d'huile d'olive\",\"Saler et poivrer\",\"Toaster les graines de sésames\",\"Ouvrir les avocats en 2, retirer le noyau et les éplucher\",\"Monter les burger en plaçant un demi-avocat face noyau vers le haut, déposer un steak, une tranche de cheddar sur le steak bien chaud pour qu'elle fonde, une rondelle de tomate, une rondelle d'oignon, quelques feuilles de salade et terminer par la seconde moitié d'avocat\",\"Parsemer quelques graines de sésames.\"],\"image\":\"http://images.marmitoncdn.org/recipephotos/multiphoto/7b/7b4e95f5-37e0-4294-bebe-cde86c30817f_normal.jpg\"}"
-    expect(@recipe.to_json).to eq exepted_string
-  end
 
 
   it 'should a m.marmiton.org url into a valid url' do

d5fa7e358e712e9fdc25cf03cc4fb79604e0e46a
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-12-06 12:41:46 +0100

    Format

--- lib/recipe_scraper.rb
+++ lib/recipe_scraper.rb
@@ -1,29 +1,22 @@
-# encoding: UTF-8
 require 'json'
 require 'open-uri'
 require 'nokogiri'
 
-require "recipe_scraper/version"
-
+require 'recipe_scraper/version'
 
 module RecipeScraper
-
   # represent a recipe fetched from an Url
   class Recipe
+    attr_reader :title, :preptime, :cooktime, :ingredients, :steps, :image
 
-    attr_reader :title, :preptime, :cooktime , :ingredients, :steps, :image
-
-    MARMITON_HOST = {desktop: 'http://www.marmiton.org/', mobile: 'http://m.marmiton.org/'}
-    G750_HOST = {desktop: 'http://www.750g.com'}
-    CUISINEAZ_HOST = {desktop: 'http://www.cuisineaz.com/'}
-
-
+    MARMITON_HOST = { desktop: 'http://www.marmiton.org/', mobile: 'http://m.marmiton.org/' }.freeze
+    G750_HOST = { desktop: 'http://www.750g.com' }.freeze
+    CUISINEAZ_HOST = { desktop: 'http://www.cuisineaz.com/' }.freeze
 
     # Instanciate a Recipe object with data crawled from an url
     #
     # @param url [String] representing an url from Marmiton or 750g website
-    def initialize url
-
+    def initialize(url)
       if marmiton_host? url
         fetch_from_marmiton url
 
@@ -34,16 +27,15 @@ module RecipeScraper
         fetch_from_cuisineaz url
 
       else
-        raise ArgumentError, "Instantiation cancelled (Host not supported)."
+        raise ArgumentError, 'Instantiation cancelled (Host not supported).'
       end
     end
-
 
     # export object properties to hash
     #
     # @return [Hash] as object's properties
     def to_hash
-      attrs = Hash.new
+      attrs = {}
       instance_variables.each do |var|
         str = var.to_s.gsub /^@/, ''
         attrs[str.to_sym] = instance_variable_get(var)
@@ -51,68 +43,60 @@ module RecipeScraper
       attrs
     end
 
-
     # convert object properties to json
     #
     # @return [String] data formated in JSON
     def to_json
-      return self.to_hash.to_json
+      to_hash.to_json
     end
 
-
     private
-
 
     # remove `\r\n` & unwanted espaces
     #
     # @param text [String] a text to sanitize
     # @return [String] as text corrected formated
-    def sanitize text
-      ['  ', '\r\n', "\r\n", "\n", "\r", "\t", / ^/, / $+/, /^  /, /^ /].each { |text_to_remove|
-        text.gsub!(text_to_remove,'')
-      }
-      return text
+    def sanitize(text)
+      ['  ', '\r\n', "\r\n", "\n", "\r", "\t", / ^/, / $+/, /^  /, /^ /].each do |text_to_remove|
+        text.gsub!(text_to_remove, '')
+      end
+      text
     end
-
 
     # test if url is from a valid marmiton.org host
     #
     # @param url [String] representing an url
     # @return [Boolean] as true if coresponding to a valid url
-    def marmiton_host? url
-      return url.include?(MARMITON_HOST[:desktop]) || url.include?(MARMITON_HOST[:mobile])
+    def marmiton_host?(url)
+      url.include?(MARMITON_HOST[:desktop]) || url.include?(MARMITON_HOST[:mobile])
     end
-
 
     # test if url is from a valid 750g.com host
     #
     # @param url [String] representing an url
     # @return [Boolean] as true if coresponding to a valid url
-    def g750_host? url
-      return url.include? G750_HOST[:desktop]
+    def g750_host?(url)
+      url.include? G750_HOST[:desktop]
     end
-
 
     # test if url is from a valid cuisineaz.com host
     #
     # @param url [String] representing an url
     # @return [Boolean] as true if coresponding to a valid url
-    def cuisineaz_host? url
-      return url.include? CUISINEAZ_HOST[:desktop]
+    def cuisineaz_host?(url)
+      url.include? CUISINEAZ_HOST[:desktop]
     end
-
 
     # fill object properties from a Marmiton url
     #
     # @param url [String] representing an url
-    def fetch_from_marmiton url
+    def fetch_from_marmiton(url)
       if marmiton_host? url
 
         url.gsub! MARMITON_HOST[:mobile], MARMITON_HOST[:desktop]
 
-        page =  Nokogiri::HTML(open(url).read)
+        page = Nokogiri::HTML(open(url).read)
         @title = page.css('h1').text
-
 
         # get times
         @preptime = page.css('div.recipe-infos__timmings__preparation > span.recipe-infos__timmings__value').text.to_i
@@ -131,20 +115,23 @@ module RecipeScraper
         end
 
         # get image
-        @image = page.css('#af-diapo-desktop-0_img').attr('src').to_s rescue NoMethodError
+        @image = begin
+                   page.css('#af-diapo-desktop-0_img').attr('src').to_s
+                 rescue StandardError
+                   NoMethodError
+                 end
 
       else
         raise ArgumentError, "Instantiation cancelled (ulr not from #{MARMITON_HOST})."
       end
     end
 
-
     # fill object properties from a 750g url
     #
     # @param url [String] representing an url
-    def fetch_from_g750 url
+    def fetch_from_g750(url)
       if g750_host? url
-        page =  Nokogiri::HTML(open(url).read)
+        page = Nokogiri::HTML(open(url).read)
         @title = page.css('h1.c-article__title').text
 
         # get times
@@ -152,14 +139,14 @@ module RecipeScraper
         @cooktime = sanitize(page.css('ul.c-recipe-summary > li.c-recipe-summary__rating[title="Temps de cuisson"]').text).to_i
 
         @steps = []
-        css_step = "div[itemprop=recipeInstructions] p"
+        css_step = 'div[itemprop=recipeInstructions] p'
         @steps = page.css(css_step).text.split /[( ),(<br>)]/
 
         @ingredients = []
-        css_ingredient = "div.c-recipe-ingredients ul.c-recipe-ingredients__list li.ingredient"
-        page.css(css_ingredient).each { |ing_node|
+        css_ingredient = 'div.c-recipe-ingredients ul.c-recipe-ingredients__list li.ingredient'
+        page.css(css_ingredient).each do |ing_node|
           @ingredients << sanitize(ing_node.text)
-        }
+        end
 
         # get image
         css_image = 'div.swiper-wrapper img.photo'
@@ -173,13 +160,12 @@ module RecipeScraper
       end
     end
 
-
     # fill object properties from a 750g url
     #
     # @param url [String] representing an url
-    def fetch_from_cuisineaz url
+    def fetch_from_cuisineaz(url)
       if cuisineaz_host? url
-        page =  Nokogiri::HTML(open(url).read)
+        page = Nokogiri::HTML(open(url).read)
         @title = page.css('h1').text
 
         # get times
@@ -187,14 +173,14 @@ module RecipeScraper
         @cooktime = page.css('#ctl00_ContentPlaceHolder_LblRecetteTempsCuisson').text.to_i
 
         @steps = []
-        page.css("#preparation p").each { |step_node|
+        page.css('#preparation p').each do |step_node|
           @steps << sanitize(step_node.text)
-        }
+        end
 
         @ingredients = []
-        page.css("section.recipe_ingredients li").each { |ing_node|
+        page.css('section.recipe_ingredients li').each do |ing_node|
           @ingredients << sanitize(ing_node.text)
-        }
+        end
 
         begin
           @image = page.css('#ctl00_ContentPlaceHolder_recipeImgLarge').attr('src').to_s
@@ -205,7 +191,5 @@ module RecipeScraper
         raise ArgumentError, "Instantiation cancelled (ulr not from #{G750_HOST})."
       end
     end
-
   end
-
 end

191342cb4e80195673acb819efa00e1ef3668c68
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-12-06 12:43:36 +0100

    Update unit tests

--- spec/recipe_750g_spec.rb
+++ spec/recipe_750g_spec.rb
@@ -1,36 +1,29 @@
 require 'spec_helper'
 
-describe "http://www.750g.com recipe crawler" do
-
+describe 'http://www.750g.com recipe crawler' do
   before(:each) do
     marmiton_url = 'http://www.750g.com/tarte-amandine-aux-pommes-dapi-r30157.htm'
     @recipe_750g = RecipeScraper::Recipe.new marmiton_url
   end
 
-
   it 'should instanciante a recipe' do
     expect(@recipe_750g).not_to be nil
   end
 
-
-
   it 'should get the right title' do
     expect(@recipe_750g.title).to eq("Tarte Amandine aux Pommes d'Api")
   end
-
 
   it 'should get the right times' do
     expect(@recipe_750g.preptime).to eq(15)
     expect(@recipe_750g.cooktime).to eq(40)
   end
 
-
   it 'should get ingredients' do
-    array_exepted = ["1 càs de sucre vanillé", "1 càs d'extrait de vanille", "30 grammes d'amandes effilées", "3 grosses pommes d'Api", "5 grammes de levure chimique", "80 grammes de sucre + un peu pour saupoudrer", "75 grammes de farine", "75 grammes de poudre d'amande", "100 grammes de beurre coupé en dés", "2 œufs"]
+    array_exepted = ['1 càs de sucre vanillé', "1 càs d'extrait de vanille", "30 grammes d'amandes effilées", "3 grosses pommes d'Api", '5 grammes de levure chimique', '80 grammes de sucre + un peu pour saupoudrer', '75 grammes de farine', "75 grammes de poudre d'amande", '100 grammes de beurre coupé en dés', '2 œufs']
     expect(@recipe_750g.ingredients).to be_kind_of(Array)
     expect(@recipe_750g.ingredients).to eq array_exepted
   end
-
 
   it 'should get steps' do
     expect(@recipe_750g.steps).to be_kind_of(Array)
@@ -39,9 +32,6 @@ describe "http://www.750g.com recipe crawler" do
 
   it 'should get picture' do
     bretzel = RecipeScraper::Recipe.new 'http://www.750g.com/bretzel-r3339.htm'
-    expect(bretzel.image).to eq src="http://static.750g.com/images/auto-427/f3aa5fb6bb6f1059c4119c5b4cb0d720/bretzel.jpeg"
+    expect(bretzel.image).to eq src = 'https://static.750g.com/images/auto-427/f3aa5fb6bb6f1059c4119c5b4cb0d720/bretzel.jpeg'
   end
-
-
-
 end
--- spec/recipe_marmiton_spec.rb
+++ spec/recipe_marmiton_spec.rb
@@ -1,60 +1,48 @@
 require 'spec_helper'
 
-describe "http://www.marmiton.org recipe crawler" do
-
+describe 'http://www.marmiton.org recipe crawler' do
   before(:each) do
     marmiton_url = 'http://www.marmiton.org/recettes/recette_burger-d-avocat_345742.aspx'
     @recipe = RecipeScraper::Recipe.new marmiton_url
   end
 
-
   it 'should create the recipe' do
     expect(@recipe).not_to be nil
   end
 
-
-
   it 'should get the right title' do
     expect(@recipe.title).to eq("Burger d'avocat")
   end
-
 
   it 'should get the right times' do
     expect(@recipe.preptime).to eq(20)
     expect(@recipe.cooktime).to eq(7)
   end
 
-
   it 'should get ingredients' do
-    array_exepted = ["2  avocats", "2  steaks hachés de boeuf", "2 tranches de cheddar", "feuille de salade", "0.5  oignon rouge", "1  tomate", "sésame", "1 filet d'huile d'olive", "1 pincée de sel", "1 pincée de poivre", "2  avocats", "2  steaks hachés de boeuf", "2 tranches de cheddar", "feuille de salade", "0.5  oignon rouge", "1  tomate", "sésame", "1 filet d'huile d'olive", "1 pincée de sel", "1 pincée de poivre"]
+    array_exepted = ['2 avocat', '2 steak hachés de boeuf', '2 tranche de cheddar', 'feuille de salade', '1/2 oignon rouge', '1 tomate', 'Sésame', "1 filet d'huile d'olive", '1 pincée de sel', '1 pincée de poivre']
     expect(@recipe.ingredients).to be_kind_of(Array)
     expect(@recipe.ingredients).to eq array_exepted
   end
-
 
   it 'should get steps' do
     expect(@recipe.steps).to be_kind_of(Array)
     expect(@recipe.steps.include?('Laver et couper la tomate en rondelles'))
   end
 
-
   it 'should get image url' do
-    expect(@recipe.image).to eq "https://image.afcdn.com/recipe/20160914/63596_w404h340c1cx2000cy3000.jpg"
+    expect(@recipe.image).to eq 'https://image.afcdn.com/recipe/20160914/63596_w420h344c1cx2000cy3000.jpg'
   end
-
 
   it 'should export all informations to an array' do
-    exepted_hash = { :cooktime => 7,
-       :image => "https://image.afcdn.com/recipe/20160914/63596_w404h340c1cx2000cy3000.jpg",
-       :ingredients => ["2  avocats", "2  steaks hachés de boeuf", "2 tranches de cheddar", "feuille de salade", "0.5  oignon rouge", "1  tomate", "sésame", "1 filet d'huile d'olive", "1 pincée de sel", "1 pincée de poivre", "2  avocats", "2  steaks hachés de boeuf", "2 tranches de cheddar", "feuille de salade", "0.5  oignon rouge", "1  tomate", "sésame", "1 filet d'huile d'olive", "1 pincée de sel", "1 pincée de poivre"],
-       :preptime => 20,
-       :steps => ["Éplucher et couper l'oignon en rondelles. Laver et couper la tomate en rondelles. Cuire les steaks à la poêle avec un filet d'huile d'olive. Saler et poivrer. Toaster les graines de sésames. Ouvrir les avocats en 2, retirer le noyau et les éplucher. Monter les burger en plaçant un demi-avocat face noyau vers le haut, déposer un steak, une tranche de cheddar sur le steak bien chaud pour qu'elle fonde, une rondelle de tomate, une rondelle d'oignon, quelques feuilles de salade et terminer par la seconde moitié d'avocat. Parsemer quelques graines de sésames."],
-       :title => "Burger d'avocat",
- }
+    exepted_hash = { cooktime: 7,
+                     image: 'https://image.afcdn.com/recipe/20160914/63596_w420h344c1cx2000cy3000.jpg',
+                     ingredients: ['2 avocat', '2 steak hachés de boeuf', '2 tranche de cheddar', 'feuille de salade', '1/2 oignon rouge', '1 tomate', 'Sésame', "1 filet d'huile d'olive", '1 pincée de sel', '1 pincée de poivre'],
+                     preptime: 20,
+                     steps: ["Éplucher et couper l'oignon en rondelles. Laver et couper la tomate en rondelles. Cuire les steaks à la poêle avec un filet d'huile d'olive. Saler et poivrer. Toaster les graines de sésames. Ouvrir les avocats en 2, retirer le noyau et les éplucher. Monter les burger en plaçant un demi-avocat face noyau vers le haut, déposer un steak, une tranche de cheddar sur le steak bien chaud pour qu'elle fonde, une rondelle de tomate, une rondelle d'oignon, quelques feuilles de salade et terminer par la seconde moitié d'avocat. Parsemer quelques graines de sésames."],
+                     title: "Burger d'avocat" }
     expect(@recipe.to_hash).to eq exepted_hash
   end
-
-
 
   it 'should a m.marmiton.org url into a valid url' do
     url = 'http://m.marmiton.org/recettes/recette_burger-d-avocat_345742.aspx'
@@ -62,6 +50,4 @@ describe "http://www.marmiton.org recipe crawler" do
     expect(recipe).not_to be nil
     expect(recipe.title).to eq("Burger d'avocat")
   end
-
-
 end
